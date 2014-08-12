6f47f881f21362b5ffb1a8e783e1d1e0353f05b4
Sean <student@epicodus.com>
2014-08-11 19:26:06 -0700

    create save method for Line class, spec passes

--- lib/lines.rb
+++ lib/lines.rb
@@ -6,4 +6,10 @@ class Line
     @name = attributes[:name]
     @id = attributes[:id]
   end
+
+  def save
+    results = DB.exec("INSERT INTO lines (name) VALUES ('#{@name}') RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
 end
--- spec/lines_spec.rb
+++ spec/lines_spec.rb
@@ -10,4 +10,13 @@ describe 'Line' do
     test_line = Line.new({:name => "Main Line"})
     expect(test_line.name).to eq "Main Line"
   end
+
+  describe "save" do
+    it 'allows you to save a line to the database' do
+      test_line = Line.new({:name => "Main Line"})
+      test_line.save
+      results = DB.exec("SELECT * FROM lines")
+      expect(test_line.id).to be_an Integer
+    end
+  end
 end
