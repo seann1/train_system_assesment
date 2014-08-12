8554954dcf989876bfd90de0d2aa302ddb3e8498
Sean <student@epicodus.com>
2014-08-11 19:19:46 -0700

    Line class initialized with a name, spec passes

--- spec/lines_spec.rb
+++ spec/lines_spec.rb
@@ -5,4 +5,9 @@ describe 'Line' do
     test_line = Line.new({:name => "Main Line"})
     expect(test_line).to be_an Line
   end
+
+  it 'returns the name of the line' do
+    test_line = Line.new({:name => "Main Line"})
+    expect(test_line.name).to eq "Main Line"
+  end
 end
