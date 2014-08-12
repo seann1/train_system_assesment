2def1c51a0742b1c05cdac6aac47fdbec08fce7d
Sean <student@epicodus.com>
2014-08-11 20:19:09 -0700

    add save method to Stop class

--- lib/stops.rb
+++ lib/stops.rb
@@ -7,4 +7,9 @@ class Stop
     @station_id = attributes[:station_id]
     @id = attributes[:id]
   end
+
+  def save
+    results = DB.exec("INSERT INTO stops (line_id, station_id) VALUES ('#{line_id}', '#{station_id}';")
+    @id = results.first['id'].to_i
+  end
 end
